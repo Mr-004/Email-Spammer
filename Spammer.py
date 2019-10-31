f9057771dde896833043b29c3a75974499db854f
Mr-004 <57210483+Mr-004@users.noreply.github.com>
2019-10-31 14:26:29 +0700

    Spammer.py

---
+++ Spammer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# - Spammer v2
+# | Description: spams a phone number by sending it a lot of sms by using Grab API
+# | Author: Mr.004
+# | Date: 
+# | Disclaimer: Editing author will not make you the real coder
+
+import spammer_class
+spammer = spammer_class.Spammer()
+spammer.author = "P4kL0nc4t"
+spammer.main()
